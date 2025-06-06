129a3e219e218519352344c41df53736b31f6483
Enzo Pacheco <enzo.pach@gmail.com>
2025-06-06 17:44:16 -0300

    Finalizando o código com as regras de negocios no service, verbos http no controller

--- src/main/java/com/enzo/cadastro_usuario/business/service/UsuarioService.java
+++ src/main/java/com/enzo/cadastro_usuario/business/service/UsuarioService.java
@@ -1,4 +1,40 @@
 package com.enzo.cadastro_usuario.business.service;
 
+import com.enzo.cadastro_usuario.infrastructure.entity.Usuario;
+import com.enzo.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void saveUsuario(Usuario usuario){
+        usuarioRepository.saveAndFlush(usuario);
+    }
+
+    public Usuario searchUsuario(Long id){
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado com ID " + id)
+        );
+    }
+
+    public void delete(Long id){
+        usuarioRepository.deleteById(id);
+    }
+
+    public void updateById(Long id, Usuario usuario){
+        Usuario usuarioEntity = searchUsuario(id);
+
+        Usuario usuarioUpdate = Usuario.builder()
+                .id(usuarioEntity.getId())
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .build();
+
+        usuarioRepository.saveAndFlush(usuarioUpdate);
+    }
 }
--- src/main/java/com/enzo/cadastro_usuario/controller/UsuarioController.java
+++ src/main/java/com/enzo/cadastro_usuario/controller/UsuarioController.java
@@ -1,4 +1,37 @@
 package com.enzo.cadastro_usuario.controller;
 
+import com.enzo.cadastro_usuario.business.service.UsuarioService;
+import com.enzo.cadastro_usuario.infrastructure.entity.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cadastro-usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveUsuario(@RequestBody Usuario usuario){
+        usuarioService.saveUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> searchById(@RequestParam Long id){
+        return ResponseEntity.ok(usuarioService.searchUsuario(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUsuarioById(@RequestParam Long id){
+        usuarioService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateUsuario(@RequestBody Long id, @RequestBody Usuario usuario){
+        usuarioService.updateById(id, usuario);
+        return ResponseEntity.ok().build();
+    }
 }
